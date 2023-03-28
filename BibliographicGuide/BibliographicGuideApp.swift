0e6c8f6f70dcd0c33854b7ad8a84af9c8b27616d
Aleksandr <pulatov.alexander@gmail.com>
2023-03-28 17:01:44 +0300

    Initial Commit

---
+++ BibliographicGuide/BibliographicGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  BibliographicGuideApp.swift
+//  BibliographicGuide
+//
+//  Created by Alexander on 28.03.23.
+//
+
+import SwiftUI
+
+@main
+struct BibliographicGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BibliographicGuide/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BibliographicGuide
+//
+//  Created by Alexander on 28.03.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
