d7401688db0762fd11557690660fdf29edcdd315
Aleksandr <pulatov.alexander@gmail.com>
2023-05-27 16:55:36 +0300

    Changed the logic of creating a pdf report

--- BibliographicGuide/Utils/Report/PdfCreator.swift
+++ BibliographicGuide/Utils/Report/PdfCreator.swift
@@ -9,7 +9,7 @@ import PDFKit
 
 class PdfCreator : NSObject {
     
-    var pag = 0
+    var page = 0
     var recordsIncludedReport: [Record] = []
     var titleReport = ""
     var creatorReport = ""
@@ -58,12 +58,38 @@ func pdfData(recordsIncludedReport: [Record], titleReport: String, creatorReport
         
         var journals = ""
         var pointer = ""
-        var ii = 1;
+        var item = 1;
         
-        for item in recordsIncludedReport{
-            journals = journals + "\t" + String(ii) + ". " + item.journalName + ". " + "\n"
-            pointer = pointer + "\t" + String(ii) + ". " + item.authors + item.title + " // " + item.journalName + " - " + String(item.year) + ", " + item.journalNumber + ", c. " + item.pageNumbers + ". \n"
-            ii += 1
+        // Сортировка записей по названию журналов
+        var newRecordsIncludedReportSort: [Record]
+        newRecordsIncludedReportSort = recordsIncludedReport.sorted(by: { $0.journalName > $1.journalName })
+        
+        var newRecordsIncludedReport = [Record]()
+        var journalName = ""
+        for record in newRecordsIncludedReportSort{
+            if(journalName == record.journalName){
+                journalName = record.journalName
+            }
+            else{
+                newRecordsIncludedReport.append(record)
+                journalName = record.journalName
+            }
+        }
+        
+        for record in newRecordsIncludedReport{
+            if(record.id != nil){
+                journals = journals + "\t" + String(item) + ". " + record.journalName + ". " + "\n"
+                item += 1
+            }
+        }
+        
+        item = 1
+        self.recordsIncludedReport.sort(by: { $0.authors < $1.authors })
+        for record in self.recordsIncludedReport{
+            if(record.id != nil){
+                pointer = pointer + "\t" + String(item) + ". " + record.authors + record.title + " // " + record.journalName + " - " + String(record.year) + ", " + record.journalNumber + ", c. " + record.pageNumbers + ". \n"
+                item += 1
+            }
         }
         
         //6
@@ -87,7 +113,6 @@ func pdfData(recordsIncludedReport: [Record], titleReport: String, creatorReport
         var image = UIImage(named: "BSTU") ?? UIImage()
         image.draw(in: textRect)
     }
-    
     
 @discardableResult
 func addText(_ text : String, context : UIGraphicsPDFRendererContext) -> CGFloat {
@@ -126,10 +151,16 @@ func addText(_ text : String, context : UIGraphicsPDFRendererContext) -> CGFloat
         // Начало новой страницы
         context.beginPage()
 
-        if(pag == 1){
+        if(page == 1){
             currentPage += 1
+            page += 1
         }
-        if(currentPage == 0 && pag == 0){
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy"
+        let newYear = dateFormatter.string(from: Date())
+
+        if(currentPage == 0 && page == 0){
             addTitle(title: "БЕЛОРУСКИЙ ГОСУДАРСТВЕННЫЙ ТЕХНОЛОГИЧЕСКИЙ", xx: 70, yy: 100)
             addTitle(title: "УНИВЕРСИТЕТ", xx: 70, yy: 120)
             
@@ -139,11 +170,11 @@ func addText(_ text : String, context : UIGraphicsPDFRendererContext) -> CGFloat
             
             addTitle(title: "Составитель: " + self.creatorReport, xx: 70, yy: 545)
             
-            addTitle(title: "Минск 2023", xx: 260, yy: 710)
+            addTitle(title: "Минск \(newYear)", xx: 260, yy: 710)
         
             addImage(xx: 260, yy: 190)
             
-            pag = 1
+            page = 1
         }
         
         //8
@@ -171,33 +202,23 @@ func addText(_ text : String, context : UIGraphicsPDFRendererContext) -> CGFloat
 
 func renderPage(_ pageNum: Int, withTextRange currentRange: CFRange, andFramesetter framesetter: CTFramesetter?) -> CFRange {
     var currentRange = currentRange
-    // Get the graphics context.
+    
     let currentContext = UIGraphicsGetCurrentContext()
 
-    // Put the text matrix into a known state. This ensures
-    // that no old scaling factors are left in place.
     currentContext?.textMatrix = .identity
 
-    // Create a path object to enclose the text. Use 72 point
-    // margins all around the text.
     let frameRect = CGRect(x: self.marginPoint.x, y: self.marginPoint.y, width: self.pageWidth - self.marginSize.width, height: self.pageHeight - self.marginSize.height)
         let framePath = CGMutablePath()
         framePath.addRect(frameRect, transform: .identity)
 
-    // Get the frame that will do the rendering.
-    // The currentRange variable specifies only the starting point. The framesetter
-    // lays out as much text as will fit into the frame.
+    // Размещает столько текста, сколько поместится в рамку.
     let frameRef = CTFramesetterCreateFrame(framesetter!, currentRange, framePath, nil)
 
-    // Core Text draws from the bottom-left corner up, so flip
-    // the current transform prior to drawing.
+    // Преобразование перед отрисовкой, так как выводит наоборот
     currentContext?.translateBy(x: 0, y: self.pageHeight)
     currentContext?.scaleBy(x: 1.0, y: -1.0)
 
-    // Draw the frame.
     CTFrameDraw(frameRef, currentContext!)
-
-    // Update the current range based on what was drawn.
     currentRange = CTFrameGetVisibleStringRange(frameRef)
     currentRange.location += currentRange.length
     currentRange.length = CFIndex(0)
