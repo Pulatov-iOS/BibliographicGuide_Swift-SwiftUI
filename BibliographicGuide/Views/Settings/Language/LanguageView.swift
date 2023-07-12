15c26f74288c5952233293ad85f3a3b03a27718b
Aleksandr <pulatov.alexander@gmail.com>
2023-07-12 22:27:31 +0300

    Fixed a display error in the language search

--- BibliographicGuide/Views/Settings/Language/LanguageView.swift
+++ BibliographicGuide/Views/Settings/Language/LanguageView.swift
@@ -15,44 +15,46 @@ struct LanguageView: View {
     
     var body: some View {
         ZStack{
-            List {
-                if(textSearch != "" && isSearching){
-                    ForEach(language.allCases.filter{ (item) -> Bool in item.title.lowercased().contains(textSearch.lowercased()) }){ item in
-                        HStack {
-                            Text(item.title)
-                            Spacer()
-                            if(languageSelectedItem.id.rawValue == item.id.rawValue){
-                                Image(systemName: "checkmark")
-                                    .fontWeight(.bold)
-                                    .foregroundColor(.blue)
+            if(!(textSearch != "" && isSearching && (language.allCases.filter{ (item) -> Bool in item.title.lowercased().contains(textSearch.lowercased()) }.count) == 0)){
+                List {
+                    if(textSearch != "" && isSearching){
+                        ForEach(language.allCases.filter{ (item) -> Bool in item.title.lowercased().contains(textSearch.lowercased()) }){ item in
+                            HStack {
+                                Text(item.title)
+                                Spacer()
+                                if(languageSelectedItem.id.rawValue == item.id.rawValue){
+                                    Image(systemName: "checkmark")
+                                        .fontWeight(.bold)
+                                        .foregroundColor(.blue)
+                                }
+                            }
+                            .contentShape(Rectangle())
+                            .onTapGesture {
+                                languageSelectedItem = item
                             }
                         }
-                        .contentShape(Rectangle())
-                        .onTapGesture {
-                            languageSelectedItem = item
+                    }
+                    else{
+                        ForEach(language.allCases){ item in
+                            HStack {
+                                Text(item.title)
+                                Spacer()
+                                if(languageSelectedItem.id.rawValue == item.id.rawValue){
+                                    Image(systemName: "checkmark")
+                                        .fontWeight(.bold)
+                                        .foregroundColor(.blue)
+                                }
+                            }
+                            .contentShape(Rectangle())
+                            .onTapGesture {
+                                languageSelectedItem = item
+                            }
                         }
                     }
                 }
-                else{
-                    ForEach(language.allCases){ item in
-                        HStack {
-                            Text(item.title)
-                            Spacer()
-                            if(languageSelectedItem.id.rawValue == item.id.rawValue){
-                                Image(systemName: "checkmark")
-                                    .fontWeight(.bold)
-                                    .foregroundColor(.blue)
-                            }
-                        }
-                        .contentShape(Rectangle())
-                        .onTapGesture {
-                            languageSelectedItem = item
-                        }
-                    }
-                }
+                .padding(.top, 82)
             }
-            .padding(.top, 82)
-            
+
             VStack(spacing: 0){
                 HStack{
                     Text("Язык")
@@ -62,15 +64,14 @@ struct LanguageView: View {
                         .padding(.bottom, 5)
                         .frame(width: UIScreen.screenWidth)
                 }
-                .background(Color(red: 0.949, green: 0.949, blue: 0.971))
                 
                 SearchBarLanguageView(textSearch: $textSearch, isSearching: $isSearching)
                     .padding(.top, 7)
                     .padding(.bottom, 5)
-                    .background(Color(red: 0.949, green: 0.949, blue: 0.971))
                 Spacer()
             }
         }
+        .background(Color(red: 0.949, green: 0.949, blue: 0.971))
     }
 }
 
